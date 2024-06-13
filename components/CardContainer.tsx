9af707c6ee97020feaf76f649ff386f72cdc037e
jmarren <jmarren99@gmail.com>
2024-06-13 15:31:09 -0700

    fixed size of panes, updated resume

--- components/CardContainer.tsx
+++ components/CardContainer.tsx
@@ -47,7 +47,7 @@ const CardContainer: React.FC<CardContainerProps> = ({ topFinal, animation, chil
   return (
     <div
       ref={cardRef}
-      className={`w-full  top-[-34vh] m-4  bg-sky-100 rounded-lg hover:shadow-sky-200 transition-all shadow-2xl gradient-shadow hover:shadow-2xl hover:scale-105 duration-300
+      className={`w-full  min-h-[300px] h-5/6 top-[-34vh] m-4  bg-sky-100 rounded-lg hover:shadow-sky-200 transition-all shadow-2xl gradient-shadow hover:shadow-2xl hover:scale-105 duration-300
         ${isMounted ? 'translate-y-0 opacity-100 visible ' : '-translate-y-full opacity-0 invisible absolute'} ${paint ? 'translate-x-full' : ''}
         `}
     >
